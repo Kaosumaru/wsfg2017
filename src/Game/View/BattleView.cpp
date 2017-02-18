07c18a1fa7712e1f48aa9973fe0822429d55aa54
Mateusz Borycki <mateuszborycki@gmail.com>
2017-02-18 16:23:17 +0100

    Update.

--- src/Game/View/BattleView.cpp
+++ src/Game/View/BattleView.cpp
@@ -5,12 +5,29 @@
 using namespace MX;
 using namespace BH;
 
+class PlayerView : public MX::Widgets::ScriptLayouterWidget
+{
+public:
+	PlayerView(const Player::pointer& player)
+	{
+		SetLayouter("Game.Player.View.Layouter");
+	}
+
+protected:
+};
+
+
 class BattleView : public MX::Widgets::ScriptLayouterWidget
 {
 public:
 	BattleView(const Player::pointer& player1, const Player::pointer& player2)
 	{
 		SetLayouter("Game.Battle.View.Layouter");
+
+		if (player1)
+			AddNamedWidget("Player1", std::make_shared<PlayerView>(player1));
+		if (player2)
+			AddNamedWidget("Player2", std::make_shared<PlayerView>(player2));
 	}
 
 protected:
